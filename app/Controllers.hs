1fd34b3faa66041c3f11aed882dc62e7fec73ff5
kubni <nikola.kuburovic123@gmail.com>
2023-10-02 15:01:12 +0200

    feat: Implement generateHiraganaCharacter
    This function almost works in its current version.
    It has a problem because of how the function show works.
    Instead of displaying a unicode japanese character, it converts
    it to ASCII, so we get something like \11235 instead of a proper
    character.

--- app/Controllers.hs
+++ app/Controllers.hs
@@ -1,27 +1,39 @@
 {-# language OverloadedStrings #-}
 module Controllers (index, generateHiraganaCharacter, missing) where
 
-import Data.Map ((!))
 import KanaTables (hiraganaTable)
 
 -- This is a "trick" that lets us keep the Text type, yet requires us to use T.func_name, so we get best of the both worlds for readability
 -- This is done in the first place because Data.Text has a lot of ambiguous conflicts with Prelude
 import Data.Text (Text)
 import qualified Data.Text as T
+
+import Data.Map ((!), keys)
 import Web.Twain
 import System.Random as Random
 import Control.Monad.IO.Class ( MonadIO(liftIO) )
 
 
 
+---- TESTING ----
+-- import GHC.IO.Encoding.UTF8
+
+
 
 --------- Actual controllers -------------
 index :: ResponderM a
--- index = send $ html "Hello World!"
-index = do
-  send $ text "Hello world!"
+index = send $ html "Hello World!"
 
-generateHiraganaCharacter = undefined
+generateHiraganaCharacter :: ResponderM a
+generateHiraganaCharacter = do
+  let columns = keys hiraganaTable
+  randomColumnInt <- liftIO $ generateRandomInteger 0 (length hiraganaTable)
+
+  let randomColumn = hiraganaTable ! (columns !! randomColumnInt)
+  randomCharInt <- liftIO $ generateRandomInteger 0 (length randomColumn)
+
+  let randomChar = randomColumn !! randomCharInt
+  send $ text $ T.pack (show randomChar)   -- FIXME: show doesn't display the character properly like putStr would and instead displays ASCII
 
 
 missing :: ResponderM a
@@ -43,4 +55,11 @@ generateRandomInteger a b = do
   let randomIntGen = createRandomIntGen randomSeed
   return $ head $ Random.randomRs (a, b) randomIntGen
 
+-- TODO: The following is equivalent
+-- generateRandomInteger = curry randomRIO
+
+-- curry :: ((a, b) -> c) -> a -> b -> c
+-- randomRIO :: (Random a, MonadIO m) => (a, a) -> m a  -- Returns a value of type Random that is encapsulated inside a MonadIO monad (or some other monad that instances MonadIO). It utilizes global pseudo-random number gen.
+-- curry randomRIO gives a function :: a -> a -> m a
+-- In our case with integers, that becomes Int -> Int -> IO Int
 ------------------------------------------
