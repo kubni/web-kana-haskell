66abb34aa8ee966d64e44f7a1a3bd4e170e4a05d
kubni <nikola.kuburovic123@gmail.com>
2023-10-03 12:54:11 +0200

    refactor: Separate random logic from controllers

--- app/Controllers.hs
+++ app/Controllers.hs
@@ -2,13 +2,13 @@
 module Controllers (index, generateHiraganaCharacter, missing) where
 
 import KanaTables (hiraganaTable)
+import RandomGenerators
 
 import Data.Text (Text)
 import qualified Data.Text as T
 
 import Data.Map ((!), keys)
 import Web.Twain
-import System.Random as Random
 import Control.Monad.IO.Class (liftIO)
 
 
@@ -36,29 +36,4 @@ missing = send $ html "Not found..."
 ------------------------------------------
 
 ---------- Helper functions --------------
-createRandomIntGen :: Int -> StdGen
-createRandomIntGen = mkStdGen
-
-getRandomSeed :: IO Int
-getRandomSeed = do
-  randomSrc <- getStdGen
-  return $ fst $ Random.random randomSrc
-
-generateRandomInteger :: Int -> Int -> IO Int
-generateRandomInteger a b = do
-  randomSeed <- getRandomSeed
-  let randomIntGen = createRandomIntGen randomSeed
-  return $ head $ Random.randomRs (a, b) randomIntGen
-
-
-
-
-
--- TODO: The following is equivalent
--- generateRandomInteger = curry randomRIO
-
--- curry :: ((a, b) -> c) -> a -> b -> c
--- randomRIO :: (Random a, MonadIO m) => (a, a) -> m a  -- Returns a value of type Random that is encapsulated inside a MonadIO monad (or some other monad that instances MonadIO). It utilizes global pseudo-random number gen.
--- curry randomRIO gives a function :: a -> a -> m a
--- In our case with integers, that becomes Int -> Int -> IO Int
 ------------------------------------------
