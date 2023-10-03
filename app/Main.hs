d93f9f3b163c6ecdcc0cdde7e9afc0deeade2766
kubni <nikola.kuburovic123@gmail.com>
2023-10-03 14:19:15 +0200

    feat: Add initial database support
    The currently chosen database is MariaDB (a fork of MySQL).
    In the future this might change, maybe even in favor of some
    NoSQL database.

--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,12 @@ module Main where
 import Network.Wai.Handler.Warp (run)
 
 import Routes (routes, routeNotFound)
+import Database.HDBC.ODBC as H (connectODBC)
 
 main :: IO ()
 main = do
+  conn <- H.connectODBC "DSN=MariaDBTest"
+
   print "Server is starting at localhost:8080. Press C-c to exit."
   run 8080 $
     foldr ($) routeNotFound routes
