8af05ab54d5c1477596cc19c63ee50f25530fd85
kubni <nikola.kuburovic123@gmail.com>
2023-10-08 11:36:26 +0200

    feat: Implement updateOne and updateMany funcs

--- app/Models.hs
+++ app/Models.hs
@@ -1,4 +1,4 @@
-module Models (Player(..), insertOne, insertMany) where
+module Models (Player(..), insertOne, insertMany, updateOne, updateMany) where
 
 import Database.HDBC.ODBC (connectODBC, Connection)
 import Database.HDBC as HDBC
@@ -36,5 +36,34 @@ insertMany players = do
   stmt <- prepare conn $ "INSERT INTO " ++ getTableName ++ " VALUES (?, ?, ?, ?)"
   let playerSqlArrays = map convertPlayerFieldValuesToSqlArray players
   executeMany stmt playerSqlArrays
+  commit conn
+  disconnect conn
+
+
+
+
+
+-- TODO: There is a lot of duplicate code here...
+-- It receives an already updated Player instance which it then just needs to use to update the actual database row.
+-- The update on the haskell data type instance will happen in the controller via Lens probably
+
+updateOne :: Player -> IO ()
+updateOne updatedPlayer = do
+  conn <- getDatabaseConnection
+  stmt <- prepare conn $ "  UPDATE " ++ getTableName ++ "\n \
+                          \ SET score=?, rank=?\n \
+                          \ WHERE id=?"
+  execute stmt [toSql $ score updatedPlayer, toSql $ rank updatedPlayer, toSql $ id_num updatedPlayer]
+  commit conn
+  disconnect conn
+
+updateMany :: [Player] -> IO ()
+updateMany updatedPlayers = do
+  conn <- getDatabaseConnection
+  stmt <- prepare conn $ "  UPDATE " ++ getTableName ++ "\n \
+                          \ SET score=?, rank=?\n \
+                          \ WHERE id=?"
+  let placeholderValues = map (\updatedPlayer -> [toSql $ score updatedPlayer, toSql $ rank updatedPlayer, toSql $ id_num updatedPlayer])  updatedPlayers
+  executeMany stmt placeholderValues
   commit conn
   disconnect conn
